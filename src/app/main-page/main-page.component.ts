d2c304da420796aa129ecc6a41a9482c59e2bf65
joaomn <97440058+joaomn@users.noreply.github.com>
2024-10-19 14:08:28 -0300

    Reefactor: mainpage

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [AvatarModule],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
 })
