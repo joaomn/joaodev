caca3a54426b0bf79d9bf4c38c14f94a3529a000
joaomn <97440058+joaomn@users.noreply.github.com>
2024-10-23 10:20:32 -0300

    REFACTOR: aboutpage adding content

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import {TranslatePipe, TranslateDirective} from "@codeandweb/ngx-translate";
+import { TranslateService } from "@codeandweb/ngx-translate";
+import { Router } from '@angular/router';
+import { CardModule } from 'primeng/card';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, TranslateDirective, CardModule, CommonModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+
+  currentLang: string | undefined;
+  isPt: boolean = false;
+
+  constructor(private translate: TranslateService, private router: Router) {
+    this.currentLang = this.translate.currentLang;
+    this.isPt = this.currentLang === 'pt';
+
+    this.translate.onLangChange.subscribe((event) => {
+      this.currentLang = event.lang;
+      this.isPt = this.currentLang === 'pt';
+    });
+  }
+
 
 }
