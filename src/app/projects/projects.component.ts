1dc3b79737b9acc04c03b571e5c5c0416ad1e019
joaomn <97440058+joaomn@users.noreply.github.com>
2024-10-23 22:16:56 -0300

    REFACTOR: additing content in projects component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import {TranslatePipe, TranslateDirective} from "@codeandweb/ngx-translate";
+import { TranslateService } from "@codeandweb/ngx-translate";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, TranslateDirective],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+  constructor(private translate: TranslateService, private router: Router) {}
 
 }
