fc5705e8d8e581e014677eae55dc521b741daf14
joaomn <97440058+joaomn@users.noreply.github.com>
2024-10-23 21:05:23 -0300

    REFACTOR: skills addinting content

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {TranslatePipe, TranslateDirective} from "@codeandweb/ngx-translate";
+import { TranslateService } from "@codeandweb/ngx-translate";
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, TranslateDirective, CommonModule, ToggleButtonModule, FormsModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
 export class SkillsComponent {
+  constructor(private translate: TranslateService, private router: Router) {}
+
+  gifit : boolean = false;
 
 }
